f0f9a32abd8195963be207ab8286d6eec1693b5a
skumar <kumasachin@gmail.com>
2025-07-12 19:25:11 +0100

    cors config update

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BasePath = import.meta.env.MODE === 'development' ? 'http://localhost:4001' : '';
+export const BasePath = import.meta.env.VITE_BASE_URL || 'http://localhost:4001';
 export const TimeConfig = {
   minorTime: 20,
   majoreTime: 60, // change to 3600 for 1 hour
