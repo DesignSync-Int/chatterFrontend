c163379719dd3ab2036343e589040a9e77ef6475
skumar <kumasachin@gmail.com>
2025-07-17 14:27:39 +0100

    Revert "email verification"
    This reverts commit 39af87b3985b06a5b948cefec9563b83d94131d7.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,10 @@ import MainLayout from '../layouts/MainLayout';
 
 const Home = lazy(() => import('../pages/home/Home'));
 const Login = lazy(() => import('../pages/login/Login'));
-const EmailVerification = lazy(
-  () => import("../pages/email/EmailVerification")
-);
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Login />} />
-    <Route path="/verify-email" element={<EmailVerification />} />
     <Route
       path="/home"
       element={
